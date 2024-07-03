209ff012fc15e96af4168149c99dec1a52e44076
EnzoClares <enzoclares1712@gmail.com>
2024-07-03 12:04:38 -0300

    uso de input, snakecase e variavel

--- app.py
+++ app.py
@@ -3,4 +3,8 @@ print('sabor express\n')#\n cria um break
 print('1. cadastrar restaurante')
 print('2. listar restaurante')
 print('3. ativar restaurante')
-print('4. sair')
+print('4. sair\n')
+
+opcao_escolhida = input('escolha uma opção: ')
+
+print('a opção escolhida foi ', opcao_escolhida)
