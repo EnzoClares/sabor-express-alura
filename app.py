d0e3801a524f0fa6ab314068d422d0392aa68e35
EnzoClares <enzoclares1712@gmail.com>
2024-07-04 16:04:38 -0300

    introducao a __name__

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os # importando uma biblioteca python
-
-print('''
+def mostrar_titulo():
+    print('''
 ░██████╗░█████╗░██████╗░░█████╗░██████╗░  ███████╗██╗░░██╗██████╗░██████╗░███████╗░██████╗░██████╗
 ██╔════╝██╔══██╗██╔══██╗██╔══██╗██╔══██╗  ██╔════╝╚██╗██╔╝██╔══██╗██╔══██╗██╔════╝██╔════╝██╔════╝
 ╚█████╗░███████║██████╦╝██║░░██║██████╔╝  █████╗░░░╚███╔╝░██████╔╝██████╔╝█████╗░░╚█████╗░╚█████╗░
@@ -10,26 +10,33 @@ print('''
       '''
 )#\n cria um break ''' aspas triplas pula linha
 
-print('1. cadastrar restaurante')
-print('2. listar restaurante')
-print('3. ativar restaurante')
-print('4. sair\n')
-
-opcao_escolhida = int(input('escolha uma opção: '))
-
-print(f'a opção escolhida foi {opcao_escolhida}')
+def mostrar_opcoes():
+    print('1. cadastrar restaurante')
+    print('2. listar restaurante')
+    print('3. ativar restaurante')
+    print('4. sair\n')
 
 def finalizar_app():
     os.system('cls')
     #os.system('clear') --> para MAC
     print('finalizando aplicativo\n')
 
+def escolher_opcoes():
+    opcao_escolhida = int(input('escolha uma opção: '))
+    print(f'a opção escolhida foi {opcao_escolhida}')
+    if opcao_escolhida == 1:
+        print('Cadastrar restaurante')
+    elif opcao_escolhida == 2:
+        print('listar restaurante')
+    elif opcao_escolhida == 3:
+        print('ativar restaurante')
+    else:
+        finalizar_app()  
 
-if opcao_escolhida == 1:
-    print('Cadastrar restaurante')
-elif opcao_escolhida == 2:
-    print('listar restaurante')
-elif opcao_escolhida == 3:
-    print('ativar restaurante')
-else:
-    finalizar_app()        
+def main():
+    mostrar_titulo()
+    mostrar_opcoes()
+    escolher_opcoes()
+
+if __name__ == '__main__':
+    main()
