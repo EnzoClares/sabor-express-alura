165654bd2ba2e0051687f793de22b7e303416d96
EnzoClares <enzoclares1712@gmail.com>
2024-07-04 15:34:48 -0300

    uso de int em input

--- app.py
+++ app.py
@@ -1,10 +1,27 @@
-print('sabor express\n')#\n cria um break
+print('''
+░██████╗░█████╗░██████╗░░█████╗░██████╗░  ███████╗██╗░░██╗██████╗░██████╗░███████╗░██████╗░██████╗
+██╔════╝██╔══██╗██╔══██╗██╔══██╗██╔══██╗  ██╔════╝╚██╗██╔╝██╔══██╗██╔══██╗██╔════╝██╔════╝██╔════╝
+╚█████╗░███████║██████╦╝██║░░██║██████╔╝  █████╗░░░╚███╔╝░██████╔╝██████╔╝█████╗░░╚█████╗░╚█████╗░
+░╚═══██╗██╔══██║██╔══██╗██║░░██║██╔══██╗  ██╔══╝░░░██╔██╗░██╔═══╝░██╔══██╗██╔══╝░░░╚═══██╗░╚═══██╗
+██████╔╝██║░░██║██████╦╝╚█████╔╝██║░░██║  ███████╗██╔╝╚██╗██║░░░░░██║░░██║███████╗██████╔╝██████╔╝
+╚═════╝░╚═╝░░╚═╝╚═════╝░░╚════╝░╚═╝░░╚═╝  ╚══════╝╚═╝░░╚═╝╚═╝░░░░░╚═╝░░╚═╝╚══════╝╚═════╝░╚═════╝░
+      '''
+)#\n cria um break ''' aspas triplas pula linha
 
 print('1. cadastrar restaurante')
 print('2. listar restaurante')
 print('3. ativar restaurante')
 print('4. sair\n')
 
-opcao_escolhida = input('escolha uma opção: ')
+opcao_escolhida = int(input('escolha uma opção: '))
 
-print('a opção escolhida foi ', opcao_escolhida)
+print(f'a opção escolhida foi {opcao_escolhida}')
+
+if opcao_escolhida == 1:
+    print('Cadastrar restaurante')
+elif opcao_escolhida == 2:
+    print('listar restaurante')
+elif opcao_escolhida == 3:
+    print('ativar restaurante')
+else:
+    print('encerrando programa')        
