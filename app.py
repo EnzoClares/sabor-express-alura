8eef2837e23823f63005c4e6e709bfc87b0f703e
EnzoClares <enzoclares1712@gmail.com>
2024-07-05 11:44:24 -0300

    uso de try e except

--- app.py
+++ app.py
@@ -21,19 +21,30 @@ def finalizar_app():
     #os.system('clear') --> para MAC
     print('finalizando aplicativo\n')
 
+def opcao_invalida():
+  print('Opção Inválida !!!')
+  input('Digite qualquer tecla para retornar ao menu\n')
+  main()
+
 def escolher_opcoes():
     opcao_escolhida = int(input('escolha uma opção: '))
     print(f'a opção escolhida foi {opcao_escolhida}')
-    if opcao_escolhida == 1:
-        print('Cadastrar restaurante')
-    elif opcao_escolhida == 2:
-        print('listar restaurante')
-    elif opcao_escolhida == 3:
-        print('ativar restaurante')
-    else:
-        finalizar_app()  
-
+    try:
+        if opcao_escolhida == 1:
+          print('Cadastrar restaurante')
+        elif opcao_escolhida == 2:
+          print('listar restaurante')
+        elif opcao_escolhida == 3:
+          print('ativar restaurante')
+        elif opcao_escolhida == 4:
+          finalizar_app()  
+        else: 
+          opcao_invalida()
+    except:
+       opcao_invalida()
+       
 def main():
+    os.system('cls')
     mostrar_titulo()
     mostrar_opcoes()
     escolher_opcoes()
